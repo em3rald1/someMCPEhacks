15efaab1b602cc9ce201ab92a28359821f87268b
em3rald1 <63785826+em3rald1@users.noreply.github.com>
2020-12-11 14:34:29 +0200

    Update hudtest.js

--- hudtest.js
+++ hudtest.js
@@ -1,16 +1,31 @@
 let GUI;
+let window__ = null;
+let lmao = false;
 
 function newLevel() {
   let ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
   ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
     try {
-      let layout = new android.widget.LinearLayout(ctx);
-      layout.setOrientation(1);
+      window__ = new android.widget.PopupWindow();
+      let layout = new android.widget.RelativeLayout(ctx);
+      let btn = new android.widget.Button(ctx);
+      btn.setOnClickListener(new android.view.View.OnClickListener({
+        onClick: function() {
+          lmao = !lmao;
+      }}));
+      layout.addView(btn);
+      let txt = new android.widget.TextView();
+      txt.setText(lmao?'':"LMAO");
+      layout.addView(txt);
       
-      let hud_button = new android.widget.Button(ctx);
-      hud_button.setText("HUD");
+      window__.setContentView(layout);
+      window__.setWidth(100);
+      window__.setHeight(100);
       
-      
+      window__.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.RED));
+      window__.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 0, 0);
+    } catch(err) {
+      print("Error: "+err); 
     }
   }}));
 }
@@ -18,9 +33,9 @@ function newLevel() {
 function leaveGame() {
   let ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
   ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
-    if(GUI != null) {
-      GUI.dismiss();
-      GUI = null;
+    if(window__ != null) {
+      window__.dismiss();
+      window__ = null;
     }
   }}))
 }
