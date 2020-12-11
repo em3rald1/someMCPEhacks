3145e817824cf703d5d1b334e16d1251ccc0a305
em3rald1 <63785826+em3rald1@users.noreply.github.com>
2020-12-11 15:15:37 +0200

    Update hudtest.js

--- hudtest.js
+++ hudtest.js
@@ -1,54 +1,42 @@
 let GUI = null;
-let lmao = false;
-let text = null;
-
-class GSLA {
-  constructor(color, gravity, ctx, width, height, layout) {
-     this.win = new android.widget.PopupWindow();
-     this.win.setContentView(layout);
-     this.win.setWidth(width);
-     this.win.setHeight(height);
-     this.win.setBackgroundDrawable(color);
-     this.win.showAtLocation(ctx.getWindow().getDecorView(), gravity, 0,0);
-  }
-  dismiss() {
-    this.win.dismiss();
-  }
-}
+let TEXT = null;
+let LMAO = false;
 
 function newLevel() {
-  let ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+  clientMessage("Greetings");
+  
+  let ctx = new com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
   ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
     try {
       let layout = new android.widget.RelativeLayout(ctx);
+      layout.setOrientation(1);
       let btn = new android.widget.Button(ctx);
-      btn.setText("Hello!");
-      btn.setOnClickListener(new android.view.View.OnClickListener({
-        onClick: function() {
-          lmao = !lmao;
-        }
-      }));
+      btn.setText("TRY");
+      btn.setOnClickListener(new android.view.View.OnClickListener({ onClick: function() {
+        LMAO = !LMAO;
+      }}))
       layout.addView(btn);
-      text = new android.widget.TextView(ctx);
-      layout.addView(text);
-      let color = new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT);
-      GUI = new GSLA(color, android.view.Gravity.TOP | android.view.Gravity.RIGHT, ctx, 200, 200, layout);
-    } catch(err) {
-      print("Error: "+err); 
+      TEXT = new android.widget.TextView(ctx);
+      layout.addView(TEXT);
+      GUI = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+      GUI.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
+      GUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 0, 0);
+    } catch(e) {
+      print("ERROR:"+e);
     }
   }}));
 }
 
 function modTick() {
-  text.setText(lmao?"":"LMAO");
+  TEXT.setText(lmao?"":"LAMO");
 }
 
 function leaveGame() {
-  let ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
-  ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
-    if(window__ != null) {
-      window__.dismiss();
-      window__ = null;
-    }
-  }}))
+  var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
+        if(GUI != null){
+            GUI.dismiss();
+            GUI = null;
+        }
+    }}));
 }
