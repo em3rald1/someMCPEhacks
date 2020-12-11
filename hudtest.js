aa85f07adf511d1c552a4660a7a6637759d7937d
em3rald1 <63785826+em3rald1@users.noreply.github.com>
2020-12-11 14:53:59 +0200

    Update hudtest.js

--- hudtest.js
+++ hudtest.js
@@ -1,33 +1,46 @@
-let GUI;
-let window__ = null;
+let GUI = null;
 let lmao = false;
+let text = null;
+
+class GUI_1 {
+  constructor(color, gravity, ctx, width, height, layout) {
+     this.win = new android.widget.PopupWindow();
+     this.win.setContentView(layout);
+     this.win.setWidth(width);
+     this.win.setHeight(height);
+     this.win.setBackgroundDrawable(color);
+     this.win.showAtLocation(ctx.getWindow().getDecorView(), gravity, 0,0);
+  }
+  dismiss() {
+    this.win.dismiss();
+  }
+}
 
 function newLevel() {
   let ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
   ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
     try {
-      window__ = new android.widget.PopupWindow();
       let layout = new android.widget.RelativeLayout(ctx);
       let btn = new android.widget.Button(ctx);
+      btn.setText("Hello!");
       btn.setOnClickListener(new android.view.View.OnClickListener({
         onClick: function() {
           lmao = !lmao;
-      }}));
+        }
+      }));
       layout.addView(btn);
-      let txt = new android.widget.TextView(ctx);
-      txt.setText(lmao?'':"LMAO");
-      layout.addView(txt);
-      
-      window__.setContentView(layout);
-      window__.setWidth(100);
-      window__.setHeight(100);
-      
-      window__.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.RED));
-      window__.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 0, 0);
+      text = new android.widget.TextView(ctx);
+      layout.addView(text);
+      let color = new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT);
+      GUI = new GUI_1(color, android.view.Gravity.TOP | android.view.Gravity.RIGHT, ctx, 200, 200, layout);
     } catch(err) {
       print("Error: "+err); 
     }
   }}));
+}
+
+function modTick() {
+  text.setText(lmao?"":"LMAO");
 }
 
 function leaveGame() {
