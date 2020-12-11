3fb815956639ba66ddc602c18a8531f7ab26bc4f
em3rald1 <63785826+em3rald1@users.noreply.github.com>
2020-12-11 14:17:18 +0200

    Update hudtest.js

--- hudtest.js
+++ hudtest.js
@@ -1,1 +1,26 @@
+let GUI;
 
+function newLevel() {
+  let ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+  ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
+    try {
+      let layout = new android.widget.LinearLayout(ctx);
+      layout.setOrientation(1);
+      
+      let hud_button = new android.widget.Button(ctx);
+      hud_button.setText("HUD");
+      
+      
+    }
+  }}));
+}
+
+function leaveGame() {
+  let ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+  ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
+    if(GUI != null) {
+      GUI.dismiss();
+      GUI = null;
+    }
+  }}))
+}
